4d703e598ebd257ca19461b70158457ea320ee30
imalyukov <imalyukov@phoenixit.ru>
2024-09-21 18:29:34 +0300

    поправил импорты

--- src/app/router/app-router.tsx
+++ src/app/router/app-router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { MainPage } from '../../pages';
+import { MainPage } from '@/pages';
 
 const AppRouter = () => (
   <Router>
