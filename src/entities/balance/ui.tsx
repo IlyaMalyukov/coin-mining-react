e86b22f26514a1504fe07a3458e4e3fa852a3a12
imalyukov <imalyukov@phoenixit.ru>
2024-09-23 22:59:09 +0300

    поправил пути к картинкам

--- src/entities/balance/ui.tsx
+++ src/entities/balance/ui.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useObserver } from 'mobx-react-lite';
 import { coinsStore } from '@/app/store';
-import { skinImage } from '@/shared';
 import './style.css';
 
 const BalanceComponent = () => {
@@ -10,7 +9,7 @@ const BalanceComponent = () => {
       <span className="balance__total">{coinsStore.totalBalance}</span>
       <img
         className="balance__coin"
-        src={skinImage[coinsStore.activeSkin]}
+        src={`/src/assets/images/${coinsStore.activeSkin}.png`}
         alt="coin"
       />
     </div>
