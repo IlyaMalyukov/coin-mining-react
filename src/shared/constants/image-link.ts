66e057c942262418ae749dd84b875b8dbf739393
imalyukov <imalyukov@phoenixit.ru>
2024-09-23 17:30:35 +0300

    добавил иконки и картинки

---
+++ src/shared/constants/image-link.ts
@@ -0,0 +1,22 @@
+import BitcoinImage from '@/assets/images/bitcoin.png';
+import EthereumImage from '@/assets/images/ethereum.png';
+import TetherImage from '@/assets/images/tether.png';
+import SolanaImage from '@/assets/images/solana.png';
+import boostIcon from '@/assets/icons/boost.svg';
+import leaderboardIcon from '@/assets/icons/leaderboard.svg';
+import skinsIcon from '@/assets/icons/skins.svg';
+
+const skinImage = {
+  'bitcoin': BitcoinImage,
+  'ethereum': EthereumImage,
+  'tether': TetherImage,
+  'solana': SolanaImage,
+};
+
+const toolbarImage = {
+  'boost': boostIcon,
+  'leaderboard': leaderboardIcon,
+  'skins': skinsIcon,
+};
+
+export { skinImage, toolbarImage };
