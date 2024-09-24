05a7aa9902f38be9aa68947073e34623e9107e95
imalyukov <imalyukov@phoenixit.ru>
2024-09-24 12:09:00 +0300

    loader

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,3 @@
+export * from './loader';
+export * from './head-title';
+export * from './back-button';
---
+++ src/shared/ui/loader/ui.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Spinner = () => {
+  return (
+    <img
+      className="icon"
+      src="/src/assets/icons/spinner.svg"
+      alt="loading"
+    />
+  );
+};
+
+export default Spinner;
