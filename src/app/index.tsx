992556c323ecfb8328f0a987200c54b1e21cb03f
imalyukov <imalyukov@phoenixit.ru>
2024-09-24 15:12:54 +0300

    уведомления об ошибках

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './style.css';
 import { AppRouter } from './router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRouter/>
+    <ToastContainer />
   </StrictMode>,
 )
--- src/pages/leaderboard-page/ui.tsx
+++ src/pages/leaderboard-page/ui.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { Loader, HeadTitle } from '@/shared';
 import { UserList } from '@/entities';
 import type { Users } from '@/entities';
@@ -9,12 +10,23 @@ const TopPlayersPage: React.FC = () => {
   const [players, setPlayers] = useState<Users | undefined>(undefined);
 
   const fetchPlayers = async () => {
+    console.log('fetch')
     try {
       const agent = getPlayersAgent();
       const bestPlayers = await agent.getBestPlayers();
       setPlayers(bestPlayers);
-    } catch (error) {
-      console.log('Error:', error);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast.error(error.message, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
     } finally {
       setIsLoading(false);
     }
